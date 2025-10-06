189280821b74acd64a9251fac2bf9e194e9c5634
Aniket Ghosh <ghoshaniket00000@gmail.com>
2025-10-07 01:03:40 +0530

    updated

--- videocall_io/components/Navbar.tsx
+++ videocall_io/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
-import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
